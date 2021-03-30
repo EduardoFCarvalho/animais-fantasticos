3e3d865e82b8b74e9fabf3f3514f38a9ea15c4fd
Eduardo <edufcarvalho@gmail.com>
2021-03-30 14:59:00 -0300

    Primeiro

---
+++ js/modules/accordion.js
@@ -0,0 +1,17 @@
+export default function initAccordion() {
+  const accorionList = document.querySelectorAll('[data-anime="accordion"] dt');
+  const activeClass = "ativo";
+  if (accorionList) {
+    accorionList[0].classList.add(activeClass);
+    accorionList[0].nextElementSibling.classList.add(activeClass);
+
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+
+    accorionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
---
+++ js/modules/anima-numeros.js
@@ -0,0 +1,31 @@
+export default function initAnimaNumeros() {
+  function animaNumeros() {
+    const numeros = document.querySelectorAll("[data-numero]");
+
+    numeros.forEach((numero) => {
+      const total = +numero.innerText;
+      const incremento = Math.floor(total / 100);
+      let start = 0;
+      const timer = setInterval(() => {
+        start = start + incremento;
+        numero.innerText = start;
+        if (start > total) {
+          numero.innerText = total; //faz o numero mesmo que passe do valor, volte ao definido
+          clearInterval(timer);
+        }
+      }, 25 * Math.random()); // Torna mais organico, fazendo crescimento aleatório
+    });
+  }
+
+  function handleMutation(mutation) {
+    if (mutation[0].target.classList.contains("ativo")) {
+      observer.disconnect();
+      animaNumeros();
+    }
+  }
+
+  const observerTarget = document.querySelector(".numeros");
+  const observer = new MutationObserver(handleMutation);
+
+  observer.observe(observerTarget, { attributes: true });
+}
