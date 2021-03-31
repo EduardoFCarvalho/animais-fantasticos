2c70c671ee4636a4159ceee9022553937531c6b6
Eduardo <edufcarvalho@gmail.com>
2021-03-31 20:44:18 -0300

    ScrollSuave transformado em classe

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,25 +1,36 @@
-export default function initScrollSuave() {
-  const linksInternos = document.querySelectorAll(
-    '[data-manu="suave"] a[href^="#"]'
-  );
+export default class ScrollSuave {
+  //Virou classe e retirou o init, não vai mais iniciar direto
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
+    if (options === undefined) {
+      this.options = { behavior: "smooth", block: "start" };
+    } else {
+      this.options = options;
+    }
+    this.scrollToSection = this.scrollToSection.bind(this); //Como alterei para classe e coloquei this.scrollToSection no addLinkEvent, aquele this passou a ser referente o link e não ao objeto, então retorna undefined e não funciona adequadamente, fazendo a referencia aqui com o .bind, consigo definir a referência que o caso é o this. Isso é fazer o bind da função callback. E com isso mantem a referencia da função para se precisar remover em algum momento conseguir, se usasse função anonima () => {} perderia referencia.
+  }
 
-  function scrollToSection(event) {
+  scrollToSection(event) {
+    // deixou de ser função para ser um método
     event.preventDefault();
     const href = event.currentTarget.getAttribute("href");
     const section = document.querySelector(href);
-    section.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-
-    //Forma Alternativa
-    // const topo = section.offsetTop;
-    // window.scrollTo({
-    //   top: topo,
-    //   behavior: 'smooth',
-    // });
+    section.scrollIntoView(this.options);
   }
-  linksInternos.forEach((link) => {
-    link.addEventListener("click", scrollToSection);
-  });
+
+  addLinkEvent() {
+    //Novo método para adicionar eventos
+    this.linksInternos.forEach((link) => {
+      link.addEventListener("click", this.scrollToSection);
+    });
+  }
+
+  init() {
+    // Nova função para iniciar
+    if (this.linksInternos.length) {
+      //para verificar se foi definido valor para links.
+      this.addLinkEvent();
+    }
+    return this; //para não retornar undefined.
+  }
 }
