c431abb584f593916878d0c8a58a0045e2b69d8c
Eduardo <edufcarvalho@gmail.com>
2021-04-05 14:51:24 -0300

    animação ao scroll refeita

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -1,25 +1,54 @@
 export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
-    this.windowMetade = window.innerHeight * 0.6;
+    this.windowMetade = window.innerHeight * 0.6; //faz com que a animação comece no meio da tela ao invés de no topo.
 
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = sectionTop - this.windowMetade < 0;
-      if (isSectionVisible) {
-        section.classList.add("ativo");
-      } else if (section.classList.contains("ativo")) {
-        section.classList.remove("ativo");
+  // O getDistance e checkDistance são uma otimização para que ao invés do caso anterior que as distâncias estavam sempre sendo atualizadas, agora o único valor que muda é o do scroll melhorando a performance.
+
+  // Pega a distância de cada item
+  // Em relação ao topo do site
+  getDistance() {
+    //Desestrutura para virar Array para poder usar o map, para poder pegar o retorno
+    this.distance = [...this.sections].map((section) => {
+      // offsetTop pega a distancia fixa do item até o topo da página.
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        // Math.floor só pra arredondar os valores de offset mas funciona sem
+        offset: Math.floor(offset - this.windowMetade),
+      };
+    });
+  }
+
+  // Verifica a distancia em cada objeto
+  // em relação ao scroll do site
+  checkDistance() {
+    this.distance.forEach((item) => {
+      // pageYOffset é a referencia do posicionamento do scroll
+      if (window.pageYOffset > item.offset) {
+        // o item.element é a section
+        item.element.classList.add("ativo");
+      } else if (item.element.classList.contains("ativo")) {
+        item.element.classList.remove("ativo");
       }
     });
   }
 
   init() {
-    this.animaScroll(); // Quando abrir o site se tiver algo a vista ele ja anima.
-    window.addEventListener("scroll", this.animaScroll);
+    if (this.sections.length) {
+      this.getDistance();
+      // Para ativar logo de inicio e o que tiver na página já ser carregado
+      this.checkDistance();
+      window.addEventListener("scroll", this.checkDistance);
+    }
+    return this;
+  }
+
+  // remove o event de Scroll
+  stop() {
+    window.removeEventListener("scroll", this.checkDistance);
   }
 }
